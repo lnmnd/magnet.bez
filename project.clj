57847a02ef27fb940d6f58a2b532aeb62c039523
lnmnd <lnmnd@users.noreply.github.com>
2014-11-05 09:53:20 +0100

    cljs REPL gehitu

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2371"]
+                 [com.cemerick/piggieback "0.1.3"]]
+  :plugins [[cider/cider-nrepl "0.7.0"]])
