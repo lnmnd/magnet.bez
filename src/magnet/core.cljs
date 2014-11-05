76711862294bb47e8ce8e07b2dd9b01cfb7b40f0
anon <nomail>
2014-11-05 15:18:35 +0100

    Erabiltzailea gehitu.

--- src/magnet/core.cljs
+++ src/magnet/core.cljs
@@ -2,20 +2,30 @@
   (:require [ajax.core :refer [GET POST PUT DELETE]]))
 
 (def aurriz "http://localhost:3000/v1/")
+
 (defonce saioa (atom {:erabiltzailea nil
                       :token nil}))
+
+(defn erabiltzailea-gehitu
+  "Erabiltzaile berri bat gehitzen du."
+  ([era pas izen]
+     (erabiltzailea-gehitu era pas izen ""))
+  ([era pas izen desk]
+     (let [param {:erabiltzailea era
+                  :pasahitza pas
+                  :izena izen}]
+       (POST (str aurriz "erabiltzaileak")
+             {:params (if (empty? desk) param (assoc param :deskribapena desk)) 
+              :format :json
+              :handler #(println %)
+              :error-handler #(println %)}))))
+
+#_(erabiltzailea-gehitu "era" "1234" "era")
 
 (GET (str aurriz "erabiltzaileak")
      {:handler #(println %)
       :error-handler #(println %)})
 
-(POST (str aurriz "erabiltzaileak")
-     {:params {:erabiltzailea "era"
-               :pasahitza "1234"
-               :izena "era"}
-      :format :json
-      :handler #(println %)
-      :error-handler #(println %)})
 
 (POST (str aurriz "saioak")
       {:params {:erabiltzailea "era"
