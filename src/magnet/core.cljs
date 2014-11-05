ec7a586eb36768940ac6aefd8f95fa6ba0c3aed0
lnmnd <lnmnd@users.noreply.github.com>
2014-11-05 10:27:17 +0100

    APIarekin eragiketa batzuk burutu

--- src/magnet/core.cljs
+++ src/magnet/core.cljs
@@ -2,7 +2,31 @@
   (:require [ajax.core :refer [GET POST PUT DELETE]]))
 
 (def aurriz "http://localhost:3000/v1/")
+(defonce saioa (atom {:erabiltzailea nil
+                      :token nil}))
 
 (GET (str aurriz "erabiltzaileak")
+     {:handler #(println %)
+      :error-handler #(println %)})
+
+(POST (str aurriz "erabiltzaileak")
+     {:params {:erabiltzailea "era"
+               :pasahitza "1234"
+               :izena "era"}
+      :format :json
+      :handler #(println %)
+      :error-handler #(println %)})
+
+(POST (str aurriz "saioak")
+      {:params {:erabiltzailea "era"
+               :pasahitza "1234"
+               :izena "era"}
+       :format :json
+       :handler #(swap! saioa assoc
+                        :erabiltzailea "era"
+                        :token (% "token"))
+       :error-handler #(println %)})
+
+(DELETE (str aurriz "erabiltzaileak/" (:erabiltzailea @saioa) "?token=" (:token @saioa))
      {:handler #(println %)
       :error-handler #(println %)})
