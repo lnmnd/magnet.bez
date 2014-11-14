70de37f6806b9d5f6e9c566319b4dfc0f4b262ba
lnmnd <lnmnd@users.noreply.github.com>
2014-11-15 00:14:07 +0100

    Funtzio laguntzailea erabili

--- src/magnet/bideak_makroak.clj
+++ src/magnet/bideak_makroak.clj
@@ -1,8 +1,8 @@
 (ns magnet.bideak.makroak)
 
+(defn bidea-eraiki [[gak bid par gor]]
+  `(secretary.core/defroute ~bid ~par
+     (cljs.core.async/put! magnet.bideak/kan [~gak ~gor])))
+
 (defmacro bideak-definitu [& bideak]
-  (cons 'do
-        (map (fn [[ gak bid par gor]]
-               `(secretary.core/defroute ~bid ~par
-                  (cljs.core.async/put! magnet.bideak/kan [~gak ~gor])))
-             bideak)))
+  (cons 'do (map bidea-eraiki bideak)))
