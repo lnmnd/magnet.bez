540f69d7dbd688052086f2d6901ccb9efb8ad9d1
lnmnd <lnmnd@users.noreply.github.com>
2014-11-30 20:02:50 +0100

    Bideak eraikitzea txukundu

--- src/magnet/bideak_makroak.clj
+++ src/magnet/bideak_makroak.clj
@@ -1,20 +1,22 @@
 (ns magnet.bideak.makroak)
 
+(defn bidea-eraiki
+  [kan gak bid & [par gor]]
+  `(secretary.core/defroute ~bid ~(or par [])
+     (cljs.core.async/put! ~kan [~gak ~(or gor nil)])))
+
 (defmacro bideak-eraiki
   "Bideak eraiki eta kanal bat itzultzen du, non bidearen balioa gehitzen den."
   [& bideak]
   (let [kan (gensym "kan")]
     `(let [~kan (cljs.core.async/chan)]
        (secretary.core/set-config! :prefix "#")
-       ~@(map (fn [x]
-                (if (= (count x) 2)
-                  `(secretary.core/defroute ~(nth x 1) []
-                     (cljs.core.async/put! ~kan [~(nth x 0) nil]))
-                  `(secretary.core/defroute ~(nth x 1) ~(nth x 2)
-                     (cljs.core.async/put! ~kan [~(nth x 0) ~(nth x 3)]))))
-              bideak)
+       
+       ~@(map #(apply bidea-eraiki kan %) bideak)
+       
        (let [history# (goog.History.)]
          (goog.events/listen history# goog.history.EventType/NAVIGATE
                              #(secretary.core/dispatch! (.-token %)))
          (.setEnabled history# true))
+       
        ~kan)))
