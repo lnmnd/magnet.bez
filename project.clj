462c46f2710fba6d85f156cb83e1125c3c7d3ecb
lnmnd <lnmnd@users.noreply.github.com>
2014-12-23 22:08:50 +0100

    1.0.0 bertsiorako prestatu

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject magnet "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject magnet "1.0.0"
+  :description "Magnet loturak: bezeroa."
+  :url "https://github.com/lnmnd/magnet.bez"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
